c7158742881adc01c11db9b385220434ca43d080
yagosys <yagosys@gmail.com>
2023-06-01 17:05:15 +0800

    root pod to access worker node

---
+++ rootpod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+nodename=$(kubectl get node -l agentpool=nested --no-headers | awk '{ print $1}')
+
+cat << EOF > privilegepod.yml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: privileged-pod
+spec:
+  hostPID: true
+  nodeSelector:
+    kubernetes.io/hostname: $nodename
+  tolerations:
+  - operator: "Exists"
+  containers:
+  - name: "netshoot"
+    image: "nicolaka/netshoot"
+    command: ["bash"]
+    stdin: true
+    tty: true
+    securityContext:
+      privileged: true
+EOF
+
+kubectl apply -f privilegepod.yml
