6f8df6a21420bb5d89dc5577ec775153675e5bad
yagosys <yagosys@gmail.com>
2023-06-05 15:28:13 +0800

    init

--- deploy_faz_container_with_slb/json_get_dns.sh
+++ deploy_faz_container_with_slb/json_get_dns.sh
@@ -1,8 +1,10 @@
 podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+service_name="fazcontainerhttps"
+ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli'
 #echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli
 # Login and get session ID
-session_id=$(curl -k -s -d '{"method":"exec","params":[{"url":"/sys/login/user","data":{"user":"admin","passwd":"Welcome.123"}}]}' -H "Content-Type: application/json" -X POST https://20.239.29.145/jsonrpc | jq -r .session)
+session_id=$(curl -k -s -d '{"method":"exec","params":[{"url":"/sys/login/user","data":{"user":"admin","passwd":"Welcome.123"}}]}' -H "Content-Type: application/json" -X POST https://$ip/jsonrpc | jq -r .session)
 
 # Create the request JSON with the session ID
 cat << EOF > request.json
@@ -19,5 +21,5 @@ cat << EOF > request.json
 EOF
 
 # Send the request
-curl -k -d @request.json -H "Content-Type: application/json" -X POST https://20.239.29.145/jsonrpc
+curl -k -d @request.json -H "Content-Type: application/json" -X POST https://$ip/jsonrpc
 
