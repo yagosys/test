32e61a5e6e9a248f0c58dbad322c80a4399a019c
yagosys <yagosys@gmail.com>
2023-06-05 12:59:59 +0800

    init

--- deploy_faz_container_with_slb/json_get_dns.sh
+++ deploy_faz_container_with_slb/json_get_dns.sh
@@ -1,3 +1,6 @@
+podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli'
+#echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli
 # Login and get session ID
 session_id=$(curl -k -s -d '{"method":"exec","params":[{"url":"/sys/login/user","data":{"user":"admin","passwd":"Welcome.123"}}]}' -H "Content-Type: application/json" -X POST https://20.239.29.145/jsonrpc | jq -r .session)
 
