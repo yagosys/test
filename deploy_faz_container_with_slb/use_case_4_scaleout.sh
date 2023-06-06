6a439783e3248c76373c29cb18b25075efa98d5d
yagosys <yagosys@gmail.com>
2023-06-06 15:53:29 +0800

    init

---
+++ deploy_faz_container_with_slb/use_case_4_scaleout.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -x
+
+
+function wait_for_faz_ready() {
+service_name="fazcontainerhttps"
+while true; do
+    ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [[ -z "$ip" || "$ip" == "<pending>" ]]; then
+        echo "Waiting for IP..."
+        sleep 10
+    else
+        echo "Public IP assigned: $ip"
+        break
+    fi
+done
+
+while true; do
+  if nc -zv $ip 443 -w 60; then
+    echo "Ping successful, breaking loop"
+    curl -k -I https://$ip
+    break
+  fi
+  sleep 2
+  kubectl get pod  -l app=fortianalyzer
+done
+}
+
+wait_for_faz_ready
+current_date=$(date '+%Y-%m-%d')
+filename="usecase_4_${current_date}.txt"
+podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+
+echo "start scale out"
+
+kubectl scale deplyment fortianalyer-deployment --replicas=2
+
+podname=$(kubectl get pod -l app=fortianalyzer | grep 0/1 | awk '{ print $1 }')
+while true; do
+  if kubectl get pod $podname | grep -q "1/1"; then
+    wait_for_faz_ready
+    echo "scale out completed"
+    break
+  fi
+  sleep 5
+done
+
+get_lb_ip
+ping_lb_publicip
+kubectl get ep 
+
+
+function get_lb_ip() {
+service_name="fazcontainerhttps"
+while true; do
+    ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [[ -z "$ip" || "$ip" == "<pending>" ]]; then
+        echo "Waiting for IP..."
+        sleep 10
+    else
+        echo "Public IP assigned: $ip"
+        break
+    fi
+done
+}
+
+function ping_lb_publicip(){
+while true; do
+#  if httping -qc1 "https://$ip"; then
+  if nc -zv $ip 443 -w 60; then
+    echo "Ping successful, breaking loop"
+    curl -k -I https://$ip
+    break
+  fi
+  sleep 2
+  kubectl get pod  -l app=fortianalyzer
+done
+}
+
+
+
