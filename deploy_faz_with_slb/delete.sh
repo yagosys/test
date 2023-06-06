4b3ad5f743d6c137afa05f81bce3d1d447c912a0
yagosys <yagosys@gmail.com>
2023-06-06 18:07:08 +0800

    init

--- deploy_faz_with_slb/delete.sh
+++ deploy_faz_with_slb/delete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 kubectl delete -f faz707vm.yaml
 kubectl delete pvc faz
-kubectl delete pvc fazlog
+kubectl delete pvc fazlogvm
 kubectl delete -f fazvmhttps.yaml
 kubectl delete -f fazlogpvc.yaml
 kubectl delete -f fazdv.yaml
