18b4bf05806285ec3557256b5941ec846cd1fe20
yagosys <yagosys@gmail.com>
2023-06-06 10:50:50 +0800

    init

--- deploy_faz_container_with_slb/user_case_2_apply_license_and_enable_api_add_fgtdevice.sh
+++ deploy_faz_container_with_slb/user_case_2_apply_license_and_enable_api_add_fgtdevice.sh
@@ -3,6 +3,27 @@ kubectl create -f ./pvc.yaml
 kubectl create -f ./fazcontainer.yaml
 kubectl create -f ./fazsvclb443.yaml
 
+function add_license_from_homedirectory_and_setup_admin_password() {
+licfile=$1
+[[ $licfile == "" ]] && licfile="VMTM23008295.lic"
+if [ ! -f "$HOME/$licfile" ]; then
+    echo "File $HOME/$licfile does not exist. put a license file in $HOME"
+    exit 1
+fi
+adminpassword="Welcome.123"
+podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+echo $podname
+
+echo cp license to container
+kubectl cp $HOME/$licfile $podname:/tmp/$licfile
+sleep 5
+echo config admin password to $adminpassword
+kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set password '$adminpassword'\n end\n" | cli'
+echo add license from $licfile
+
+kubectl exec -it $podname -- /bin/bash -c 'echo -e "execute add-vm-license \"$(cat /tmp/'$licfile')\"" | cli'
+}
+
 function wait_for_faz_ready() {
 service_name="fazcontainerhttps"
 while true; do
@@ -26,4 +47,9 @@ while true; do
   kubectl get pod  -l app=fortianalyzer
 done
 }
+
 wait_for_faz_ready
+add_license_from_homedirectory_and_setup_admin_password
+echo after apply license,faz will restart , wait for faz ready again
+wait_for_faz_ready
+
