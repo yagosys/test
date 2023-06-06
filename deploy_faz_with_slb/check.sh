55fb62587d2544f3fece47e9111a11179fc5ecf2
yagosys <yagosys@gmail.com>
2023-06-06 18:24:24 +0800

    init

--- deploy_faz_with_slb/check.sh
+++ deploy_faz_with_slb/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-filename="fazvmhttps.yml"
-
 service_name="fazvmhttps"
+applabel="faz"
+
 
 while true; do
     ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
@@ -17,16 +17,18 @@ done
 
 dateStart=$(date)
 while true; do
-  if httping -qc1 "https://$ip"; then
+#  if httping -qc1 "https://$ip"; then
+  if nc -zv $ip 443 -w 60; then
     echo "Ping successful, breaking loop"
     curl -k -I https://$ip
     break
   fi
   sleep 2
-  kubectl get pod
+  kubectl get pod  -l app=$applabel
 done
 dateStop=$(date)
-
-echo faz bootup record > result.txt
-echo boot at $dateStart >> result.txt
-echo service ready at $dateStop  >> result.txt
+current_date=$(date '+%Y-%m-%d')
+filename="$1_faz_boot_time_${current_date}.txt"
+echo faz bootup record > $filename 
+echo boot at $dateStart >> $filename
+echo service ready at $dateStop  >> $filename
