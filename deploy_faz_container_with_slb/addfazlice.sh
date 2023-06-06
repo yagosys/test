a5f54763fe36f59d5856b2dc52e530ff54e53753
yagosys <yagosys@gmail.com>
2023-06-06 08:01:05 +0800

    add set admin password

--- deploy_faz_container_with_slb/addfazlice.sh
+++ deploy_faz_container_with_slb/addfazlice.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 licfile="VMTM23008295.lic"
+adminpassword="Welcome.123"
 podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
 kubectl cp $HOME/$licfile $podname:/tmp/$licfile
-sleep 20
+sleep 5
+kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set password '$adminpassword'\n end\n" | cli'
 kubectl exec -it $podname -- /bin/bash -c 'echo -e "execute add-vm-license \"$(cat /tmp/'$licfile')\"" | cli'
