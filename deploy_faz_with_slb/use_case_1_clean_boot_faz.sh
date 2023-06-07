8243a9375e10d05c51ac0603138dfdf0f90f6d22
yagosys <yagosys@gmail.com>
2023-06-07 14:07:02 +0800

    init

--- deploy_faz_with_slb/use_case_1_clean_boot_faz.sh
+++ deploy_faz_with_slb/use_case_1_clean_boot_faz.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 #record the time took from  create faz to faz able to accept log
 #this will start from create storage for faz and use curl to access faz 443 port as end
+kubectl create -f cidatadv.yaml
 kubectl create -f fazlogpvc.yaml &&
 kubectl create -f fazdv.yaml &&
 kubectl create -f faz707vm.yaml &&
@@ -9,4 +10,5 @@ kubectl create -f fazvmhttps.yaml &&
 kubectl delete -f faz707vm.yaml
 kubectl delete pvc faz
 kubectl delete pvc fazlogvm
+kubectl delete pvc cidata
 kubectl delete -f fazvmhttps.yaml
