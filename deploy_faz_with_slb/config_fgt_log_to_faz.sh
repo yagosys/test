66866500107aabaf4594c28c8fcc36b6572536fe
yagosys <yagosys@gmail.com>
2023-06-07 16:15:36 +0800

    init

---
+++ deploy_faz_with_slb/config_fgt_log_to_faz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo $ip
+fazsn=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  $ip 'get system status' | grep "Serial Number" | cut -d ':' -f 2 | tr -d ' ')
+echo $fazsn
+fgtip=$1
+echo $fgtip
+[[ -z $fgtip ]] &&  fgtip="52.246.137.113"
+echo $fazcn
+cat << EOF  
+config log fortianalyzer setting
+    set status enable
+    set server $fgtip
+    set serial $fazsn
+end
+EOF
