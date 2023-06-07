c46401244bac442f83981be2157b70b3e8537df5
yagosys <yagosys@gmail.com>
2023-06-07 16:59:00 +0800

    init

--- deploy_faz_with_slb/faz722bigbang/check.sh
+++ deploy_faz_with_slb/faz722bigbang/check.sh
@@ -34,7 +34,7 @@ echo faz bootup record > $filename
 echo boot at $dateStart >> $filename
 echo service ready at $dateStop  >> $filename
 echo -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@$ip  'diag cdb upgrade summary' | tee -a $filename
-ssh admin@$ip 'diag cdb upgrade summary' | tee -a $filename
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@$ip 'diag cdb upgrade summary' | tee -a $filename
 echo -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@$ip 'diag faz-cdb upgrade summary' | tee -a $filename
-ssh admin@$ip 'diag faz-cdb upgrade summary' | tee -a $filename
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@$ip 'diag faz-cdb upgrade summary' | tee -a $filename
 echo upgrade done
