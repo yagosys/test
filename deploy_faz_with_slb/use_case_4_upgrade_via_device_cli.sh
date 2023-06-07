c738ac27d677c173ab36edfbab6fad522f170628
yagosys <yagosys@gmail.com>
2023-06-07 18:03:46 +0800

    init

--- deploy_faz_with_slb/use_case_4_upgrade_via_device_cli.sh
+++ deploy_faz_with_slb/use_case_4_upgrade_via_device_cli.sh
@@ -9,6 +9,6 @@ echo $image_name
 
 ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 # Assuming "execute restore image" is a command
-ssh admin@$ip "execute restore image scp $image_name \"$1\" \"$2\" \"$3\""
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  admin@$ip "execute restore image scp $image_name \"$1\" \"$2\" \"$3\""
 
 
