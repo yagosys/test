427935e1ea400aa080fa7bf13f6bb9e24e584cdd
yagosys <yagosys@gmail.com>
2023-06-08 10:51:15 +0800

    init

---
+++ install_tools_for_azshell.sh
@@ -0,0 +1,13 @@
+cd $HOME
+mkdir $HOME/bin -p
+curl -OfL https://udomain.dl.sourceforge.net/project/netcat/netcat/0.7.1/netcat-0.7.1.tar.gz
+tar xvf netcat-0.7.1.tar.gz
+cd netcat-0.7.1
+./configure --prefix=$(pwd)
+make && cp ./src/netcat ./../nc 
+cd ./../
+./nc --version
+cp ./nc $HOME/bin
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/bin | tee -a $HOME/.bashrc
+source $HOME/.bashrc
