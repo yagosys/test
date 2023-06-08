acd77893b60e795819fe2ce7ffa2d7712dbfbe0e
yagosys <yagosys@gmail.com>
2023-06-08 11:04:40 +0800

    init

--- downoad_install_netcat.sh
+++
@@ -1,9 +0,0 @@
-curl -OfL https://udomain.dl.sourceforge.net/project/netcat/netcat/0.7.1/netcat-0.7.1.tar.gz
-tar xvf netcat-0.7.1.tar.gz
-cd netcat-0.7.1
-./configure --prefix=$(pwd)
-make && cp ./src/netcat ./../nc 
-cd ./../
-./nc --version
-cp ./nc $HOME/bin
-export PATH=%$PATH:$HOME/bin
