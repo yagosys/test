33e08f8cf46d412b6dade6f417e4b39e0a98654b
yagosys <yagosys@gmail.com>
2023-06-09 09:27:52 +0800

    init

--- deploy_faz_with_slb/faz722bigbang/demo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x 
-
-kubectl create -f cidatadv.yaml  
-kubectl create -f faz722dv.yaml 
-kubectl create -f faz722vm.yaml 
--- deploy_faz_with_slb/faz722bigbang/demo_bigbang.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-kubectl delete -f faz707vm.yaml
-kubectl delete pvc faz
-echo done
-kubectl create -f faz722dv.yaml
-kubectl create -f faz722vm.yaml
-./check.sh usecase3
-
