987519fc2ce1cb7bde44f8ea755b073b3cd835e5
yagosys <yagosys@gmail.com>
2023-06-09 09:25:41 +0800

    init

--- deploy_faz_with_slb/replace_faz_volume.sh
+++
@@ -1,1 +0,0 @@
-kubectl delete -f faz707temp.yaml && kubectl delete pvc faz && kubectl delete pvc cidata && kubectl create -f cidatadv.yaml  && kubectl create -f fazdv.yaml && kubectl create -f faz707temp.yaml
