a6b0c518ac1f1b6ee702140619433d2128fcce63
yagosys <yagosys@gmail.com>
2023-06-13 20:34:13 +0800

    init

--- deploy_faz_container_with_slb/use_case_2_apply_license_and_enable_api.sh
+++ deploy_faz_container_with_slb/use_case_2_apply_license_and_enable_api.sh
@@ -2,6 +2,7 @@
 kubectl create -f ./pvc.yaml
 kubectl create -f ./fazcontainer.yaml
 kubectl create -f ./fazsvclb443.yaml
+namespace="default"
 
 function add_license_from_homedirectory_and_setup_admin_password() {
 licfile=$1
@@ -11,24 +12,24 @@ if [ ! -f "$HOME/$licfile" ]; then
     exit 1
 fi
 adminpassword="Welcome.123"
-podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+podname=$(kubectl get -n $namespace pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
 echo $podname
 
 echo cp license to container
-kubectl cp $HOME/$licfile $podname:/tmp/$licfile
+kubectl cp $HOME/$licfile $namespace/$podname:/tmp/$licfile
 sleep 5
-echo config admin password to $adminpassword
-kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set password '$adminpassword'\n end\n" | cli'
+#echo config admin password to $adminpassword
+#kubectl -n $namespace exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set password '$adminpassword'\n end\n" | cli'
 echo add license from $licfile
 
-kubectl exec -it $podname -- /bin/bash -c 'echo -e "execute add-vm-license \"$(cat /tmp/'$licfile')\"" | cli'
+kubectl -n $namespace exec -it $podname -- /bin/bash -c 'echo -e "execute add-vm-license \"$(cat /tmp/'$licfile')\"" | cli' | tee -a $filename
 }
 
 function enable_api_for_admin() {
-podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+podname=$(kubectl get -n $namespace pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
 service_name="fazcontainerhttps"
-ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
-kubectl exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli'
+ip=$(kubectl get -n $namespace svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+kubectl -n $namespace exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli'
 #echo -e "config system admin user \n edit admin\n set rpc-permit read-write\n end\n" | cli
 # Login and get session ID
 session_id=$(curl -k -s -d '{"method":"exec","params":[{"url":"/sys/login/user","data":{"user":"admin","passwd":"Welcome.123"}}]}' -H "Content-Type: application/json" -X POST https://$ip/jsonrpc | jq -r .session)
@@ -48,13 +49,13 @@ cat << EOF > request.json
 EOF
 
 # Send the request
-curl -k -d @request.json -H "Content-Type: application/json" -X POST https://$ip/jsonrpc
+curl -k -d @request.json -H "Content-Type: application/json" -X POST https://$ip/jsonrpc | tee -a $filename
 }
 
 function wait_for_faz_ready() {
 service_name="fazcontainerhttps"
 while true; do
-    ip=$(kubectl get svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    ip=$(kubectl get -n $namespace svc $service_name --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
     if [[ -z "$ip" || "$ip" == "<pending>" ]]; then
         echo "Waiting for IP..."
         sleep 10
@@ -71,28 +72,37 @@ while true; do
     break
   fi
   sleep 2
-  kubectl get pod  -l app=fortianalyzer
+  kubectl get -n $namespace pod  -l app=fortianalyzer
 done
 }
 
+current_date=$(date '+%Y-%m-%d-%H-%M')
+filename="use_case_2_${current_date}.txt"
+echo start deploy faz at $(date) > $filename
 wait_for_faz_ready && 
+echo faz ready at $(date) | tee -a $filename
+kubectl get -n $namespace pod -l app=fortianalyzer | tee -a $filename
+echo start apply license | tee -a $filename
 add_license_from_homedirectory_and_setup_admin_password &&
-echo after apply license,faz will restart , wait for faz ready again
-echo sleep 60 for faz to reboot
+echo after apply license,faz will restart , wait for faz ready again  | tee -a $filename
+echo sleep 60 for faz to reboot | tee -a $filename 
 sleep 60
 wait_for_faz_ready
-current_date=$(date '+%Y-%m-%d')
-filename="usercase_2_${current_date}.txt"
-echo "license applied" > $filename
-podname=$(kubectl get pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
+echo "license applied" | tee -a  $filename
+echo faz ready again after reboot at $(date) | tee -a $filename
+kubectl get -n $namespace pod -l app=fortianalyzer | tee -a $filename
+podname=$(kubectl get -n $namespace pod -l app=fortianalyzer | grep Running | awk '{ print $1 }')
 echo "$pod_name user admin has password $adminpassword" >> $filename
 
 echo "use cli to get system status" >> $filename
-kubectl exec -it $podname -- /bin/bash -c 'echo -e "get system status" | cli' | tee -a  $filename
+kubectl -n $namespace exec -it $podname -- /bin/bash -c 'echo -e "get system status" | cli' | tee -a  $filename
+kubectl -n $namespace exec -it $podname -- /bin/bash -c 'top -n 1' | tee -a  $filename
 
-echo "start enable json rpc api for $podname" >> $filename
+echo "start enable json rpc api for $podname" | tee -a  $filename
 
-enable_api_for_admin && echo "admin user json rpc api enable" >> $filename
+adminpassword="Welcome.123"
+kubectl -n $namespace exec -it $podname -- /bin/bash -c 'echo -e "config system admin user \n edit admin\n set password '$adminpassword'\n end\n" | cli' | tee -a $filename
+enable_api_for_admin && echo "admin user json rpc api enable"| tee -a  $filename
 
-
+for i in $(seq 1 5); do echo -e '\a'; sleep 1; done 
 
