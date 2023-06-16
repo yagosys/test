2cce73db79fde94f1717044f0547cd3ad8f05152
yagosys <yagosys@gmail.com>
2023-06-16 18:30:12 +0800

    bring up cfaz and cfmg together with kong as ingress controller

---
+++ use_case_2_cfaz_cfmg_kong_ingress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+currentdir=$(pwd)
+
+cd deploy_faz_container_with_slb/faz_kong_ingress_with_domainname_demo
+
+./step1-demo_faz_container_enable_port80_disable_redirect.sh && \
+./step2_create_kong_with_static_ip.sh  && \
+./step3_install_cert_mgmt.sh && \
+./step4_deploy_ingressrule_for_fortianalyzer.sh  
+
+cd $currentdir
+
+cd deploy_fmg_container_with_slb/fmg_kong_ingress_with_domainname_demo
+
+./step1-demo_fmg_container_enable_port80_disable_redirect.sh && \
+./step2_create_kong_with_static_ip.sh  && \
+./step3_install_cert_mgmt.sh && \
+./step4_deploy_ingressrule_for_fortimanager.sh  
+
+
+
+
+
---
+++ use_case_2_delete_all.sh
@@ -0,0 +1,1 @@
+kubectl delete namespace fortimanager && kubectl delete namespace fortianalyzer && kubectl delete namespace kong && kubectl delete namespace cert-manager
