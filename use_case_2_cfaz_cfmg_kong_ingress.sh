939956c674bacc66fe1183b8b938309770c8c4e7
yagosys <yagosys@gmail.com>
2023-06-19 17:01:36 +0800

    change to create cfaz_cfmg at same time

--- use_case_2_cfaz_cfmg_kong_ingress.sh
+++ use_case_2_cfaz_cfmg_kong_ingress.sh
@@ -8,7 +8,7 @@ cd deploy_faz_container_with_slb/faz_kong_ingress_with_domainname_demo
 ./step1-demo_faz_container_enable_port80_disable_redirect.sh && \
 ./step2_create_kong_with_static_ip.sh  && \
 ./step3_install_cert_mgmt.sh && \
-./step4_deploy_ingressrule_for_fortianalyzer.sh  
+./step4_deploy_ingressrule_for_fortianalyzer.sh   &
 
 cd $currentdir
 
@@ -17,7 +17,9 @@ cd deploy_fmg_container_with_slb/fmg_kong_ingress_with_domainname_demo
 ./step1-demo_fmg_container_enable_port80_disable_redirect.sh && \
 ./step2_create_kong_with_static_ip.sh  && \
 ./step3_install_cert_mgmt.sh && \
-./step4_deploy_ingressrule_for_fortimanager.sh  
+./step4_deploy_ingressrule_for_fortimanager.sh  &
+
+wait 
 
 
 
