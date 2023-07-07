cd903593b93c296bba260891d3baab77abd96577
yagosys <yagosys@gmail.com>
2023-07-07 09:50:36 +0800

    init

--- use_case_3_cfaz_cfmg_nginx_ingress_westus2.sh
+++ use_case_3_cfaz_cfmg_nginx_ingress_westus2.sh
@@ -5,16 +5,15 @@ currentdir=$(pwd)
 
 cd deploy_faz_container_with_slb/nginx_ingress_demo_faz
 
-./demo_westus2_faztest.sh &
+./demo_westus2_faztest.sh 
 
 cd $currentdir
 
 cd deploy_fmg_container_with_slb/nginx_ingress_demo_fmg
-./demo_westus2_fmgtest.sh &
+./demo_westus2_fmgtest.sh 
 
 cd $currentdir
 
-wait 
 
 
 
