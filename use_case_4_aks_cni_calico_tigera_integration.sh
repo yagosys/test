e41b1cccb1883eb2db9c1166363818cea81519a7
yagosys <yagosys@gmail.com>
2023-07-11 20:42:41 +0800

    init

--- use_case_4_aks_cni_calico_tigera_integration.sh
+++ use_case_4_aks_cni_calico_tigera_integration.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 echo before continue, please go to calicocloud.io to get license and update file
 echo windows/tigera/00_please_update_before_run_register_to_calicocloud.sh
-sleep 30
-read -p "Do you wish to continue? [yes/no]: " answer
+echo if no input given, the script will continue after 60 secons
+sleep 10 
+if ! read -t 60 -p "Do you wish to continue? [yes/no]: " answer; then
+  answer="yes"
+fi
+# If no input was given, default to "yes"
+if [ -z "$answer" ]; then
+  answer="yes"
+fi
 if [[ "$answer" == 'yes' ]]; then
     echo "You chose to continue. The script will proceed..."
     # Add your additional script code here
