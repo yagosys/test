700625ac0fd512a2c93d1bb061d625c3154b1856
yagosys <yagosys@gmail.com>
2023-07-11 18:07:49 +0800

    init

---
+++ use_case_4_aks_cni_calico_tigera_integration.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+echo before continue, please go to calicocloud.io to get license and update file
+echo windows/tigera/00_please_update_before_run_register_to_calicocloud.sh
+sleep 30
+read -p "Do you wish to continue? [yes/no]: " answer
+if [[ "$answer" == 'yes' ]]; then
+    echo "You chose to continue. The script will proceed..."
+    # Add your additional script code here
+       currentdir=$(pwd)
+       cd windows
+       ./create_aks_and_ubuntu_win_node_westus2_azure_cni.sh
+       cd fortigate
+        ./create_fortigate_vm_with_subnet.sh
+        cd ./../
+        cd tigera
+       ./demo.sh
+else
+    echo "You chose not to continue. The script will exit."
+    exit 1
+fi
